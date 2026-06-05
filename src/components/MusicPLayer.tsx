3a875b31088e91e41dd9784c7535236665f512b0
Thales Carvalho <selath10@gmail.com>
2026-06-05 16:50:13 -0300

    progress bar logic added

--- src/components/MusicPLayer.tsx
+++ src/components/MusicPLayer.tsx
@@ -2,40 +2,138 @@ import { useEffect, useRef } from "react";
 import useMusic from "../hooks/useMusic";
 
 function MusicPlayer(){
-    const {currentTrack, formatTime, currentTime, duration} = useMusic();
-    const audioRef = useRef(null);
-    
+
+    const {
+        currentTrack,
+        formatTime,
+        currentTime,
+        setCurrentTime,
+        duration,
+        setDuration,
+        nextTrack,
+        prevTrack,
+        pause,
+        play,
+        isPlaying,
+    } = useMusic();
+
+
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const handleTimeChange = (e: any) =>{
+        const audio = audioRef.current;
+        if(!audio){
+            return
+        }
+        
+        // get the timevalue at the selected position
+        const newSelectedTime = e.target.value;
+        //Pass the new timw to the audio reference
+        audio.currentTime = newSelectedTime;
+        //update the progressbar 
+        setCurrentTime(newSelectedTime);
+
+
+
+
+
+    }
+
     useEffect(()=>{
+      
         const audio = audioRef.current;
         if(!audio){
             return
         }
 
+        if (isPlaying) {
+            audio.play().catch((err)=> console.error(err));
+        }else{
+            audio.pause();
+        }
 
 
-    },[])
+    },[isPlaying])//Whenever iSplaying changes value , this useEfect runs
+
+    
+    useEffect(()=>{
+
+        const audio = audioRef.current;
+
+        if(!audio){
+            return
+        }
+
+        //Run logic whenever the metadate for the audio loads
+        const handleLoadedMetadata = ()=>{
+           setDuration(audio.duration);
+        }
+
+        const handleTimeUpdate = ()=>{
+            // currentTime here is a property of the audio reference
+            setCurrentTime(audio.currentTime);
+        }
+        const handleEnded = ()=>{
+            nextTrack();
+        }
+
+        //Subscripitons
+        audio.addEventListener('loadedmetadata', handleLoadedMetadata);
+        audio.addEventListener('timeupdate', handleTimeUpdate);
+        audio.addEventListener('ended', handleEnded);
+
+        //clean subscription
+        return()=>{
+            audio.removeEventListener('loadedmetadata', handleLoadedMetadata);
+            audio.removeEventListener('timeupdate', handleLoadedMetadata);
+            audio.removeEventListener('ended', handleEnded);
+        };
+
+
+    },[setDuration,setCurrentTime,currentTrack]
+    ) // Dependecies goes here, and with the empty array means it only runs once
+    // if you have functions in the array, means the useEffect will run every time there is a change
+    //in that parameter 
 
 
     return (
         <div className="music-player">
-            <audio ref={audioRef} preload="metadata" crossOrigin="anonymous"></audio>
+            <audio 
+                ref={audioRef} 
+                src={currentTrack.url} 
+                preload="metadata" 
+                crossOrigin="anonymous">    
+            </audio>
 
             <div className="track-info">
                 <h3>{currentTrack.title}</h3>
                 <p>{currentTrack.artist}</p>
             </div>
+
             <div className="progress-container">
                 <span className="time">{formatTime(currentTime)}</span>
-                <input type="range" 
+                <input 
+                    type="range" 
                     min='0' 
                     max={duration || 0} 
                     step='0.1' 
                     value={currentTime || 0 }
                     className="progress-bar"
+                    onChange={handleTimeChange}
                     //style={}
                 />
-                <span className="duration">{formatTime(duration)}</span>
+                <span className="time">{formatTime(duration)}</span>
             </div>
+
+            <div className="controls">
+                <button className="control-btn" onClick={prevTrack}>⏮</button>
+                <button className="control-btn play-btn" onClick={()=> isPlaying ? pause() : play()}>
+                    {isPlaying ? '⏸' : '▶'}
+                </button>
+                <button className="control-btn" onClick={nextTrack}>▶⏭</button>
+            </div>
+
+
         </div>
     )
 }
--- src/hooks/useMusic.tsx
+++ src/hooks/useMusic.tsx
@@ -6,48 +6,48 @@ const songs = [
         title: 'Omah Lay - Crazy Love',
         artist: 'Omah Lay',
         url: '/Songs/Omah Lay - Crazy Love.mp3',
-        duration: '2:20'
+        duration: '3:56'
     },
     {
         id: 2,
         title: 'Omah Lay - Feelings',
         artist: 'Omah Lay',
-        url: '/Songs/Omah Lay - Feelings.mp3',
+        url: '/Songs/Omah Lay – Feelings.mp3',
         duration: '2:20'
     },
     {
         id: 3,
         title: 'Omah Lay - Off & On',
         artist: 'Omah Lay',
-        url: '/Songs/Omah Lay - Off & On.mp3',
+        url: '/Songs/Omah Lay – Off & On.mp3',
         duration: '2:20'
     },
     {
         id: 4,
         title: 'Omah Lay - Pains',
         artist: 'Omah Lay',
-        url: '/Songs/Omah Lay - Pains.mp3',
+        url: '/Songs/Omah Lay – Pains.mp3',
         duration: '2:20'
     },
     {
         id: 5,
         title: 'Omah Lay - Pray',
         artist: 'Omah Lay',
-        url: '/Songs/Omah Lay - Pray.mp3',
+        url: '/Songs/Omah Lay – Pray.mp3',
         duration: '2:20'
     },
     {
         id: 6,
         title: 'Omah Lay - Slow Poison',
         artist: 'Omah Lay',
-        url: '/Songs/Omah Lay - Slow Poison.mp3',
+        url: '/Songs/Omah Lay – Slow Poison.mp3',
         duration: '2:20'
     },
     {
         id: 7,
         title: 'Omah Lay - Body & Vibe',
         artist: 'Omah Lay',
-        url: '/Songs/Omah Lay - Body & Vibe.mp3',
+        url: '/Songs/Omahlay - Body & Vibe.mp3',
         duration: '2:20'
     }
       
@@ -58,15 +58,38 @@ function useMusic(){
 
     const[ allSongs, setAllSongs ] = useState(songs);
     const [currentTrack, setCurrentTrack] = useState(songs[0]);
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentTrackIndex] = useState(0);
     const [currentTime , setCurrentTime] = useState(0);
     const [duration , setDuration] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     //HandlePLay Function
     const handlePlaySong = (song:any, index: number)=>{
         setCurrentTrack(song);
-        setCurrentIndex(index);
+        setCurrentTrackIndex(index);
         
+    }
+
+    const nextTrack = () => {
+       
+        setCurrentTrackIndex((prev) => {
+            const nextIndex = (prev + 1) % allSongs.length;
+            setCurrentTrack(allSongs[nextIndex]);
+            return nextIndex;
+        })
+        setIsPlaying(false);
+    }
+
+    const prevTrack = () => {
+        
+        setCurrentTrackIndex((prev) => {
+            const nextIndex = prev === 0 ? allSongs.length - 1 : prev - 1 ;
+            setCurrentTrack(allSongs[nextIndex]);
+            return nextIndex;
+        })
+
+        setIsPlaying(false);
+
     }
 
     const formatTime = (time:any)=>{
@@ -82,18 +105,27 @@ function useMusic(){
 
     }
 
+    const play = () => setIsPlaying(true);
+    const pause = () => setIsPlaying(false);
 
     
     return {
         allSongs,
         handlePlaySong,
         currentTrack,
+        setCurrentTrackIndex,
         currentIndex,
-        setCurrentIndex,
         setCurrentTime,
         currentTime,
         formatTime,
-        duration
+        duration,
+        setDuration,
+        nextTrack,
+        prevTrack,
+        play,
+        pause,
+        isPlaying,
+        setIsPlaying
     };
 
 }    
